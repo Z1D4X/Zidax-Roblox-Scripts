b8ec43467066b6d6e59c44507967a2693fc3fe07
Zidax <88258438+Z1D4X@users.noreply.github.com>
2022-08-22 10:24:29 +0800

    Update and rename HRS_Farm.lua to CDT Auto Rally.lua

---
+++ CDT Auto Rally.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 1554960397 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Z1D4X/Zidax-Roblox-Scripts/main/Car_Dealership_Tycoon_AutoRally.lua"))()
+else
+    game.Players.LocalPlayer:Kick("Game Not Supported")
+end
--- HRS_Farm.lua
+++
@@ -1,3 +0,0 @@
-getgenv().AutoHomeRun = true
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Z1D4X/Zidax-Home-Run-Sim/main/Home_Run_Sim_Farm.lua"))()
